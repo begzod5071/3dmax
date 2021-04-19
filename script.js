ab09ef8f2432a3f976043b5728f301be80b582e7
begzod5071 <rafikovbegzod5071@gmail.com>
2021-04-19 14:42:24 +0500

    finished site

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var $_ = function (selector, node = document) {
+  return node.querySelector(selector);
+};
+
+const videos = [
+  {id: 1, title: "1-Dars", src: "https://www.youtube.com/embed/2R89ZT9jSBU"},
+  {id: 2, title: "2-Dars", src: "https://www.youtube.com/embed/2R89ZT9jSBU"},
+]
+
 $('[data-fancybox="gallery"]').fancybox({
   buttons : [
     'close'
@@ -10,6 +19,24 @@ $('[data-fancybox="gallery"]').fancybox({
 const SiteHeader = document.querySelector(".site-header")
 const Toggle = document.querySelector(".site-header__menu")
 
+
+
 Toggle.addEventListener("click", e => {
   SiteHeader.classList.toggle("site-header--open")
 })
+
+const ListVideo = document.querySelector(".main-videos__list")
+const TemplateVideo = document.querySelector(".video-template").content;
+var elVideoFragment = document.createDocumentFragment();
+
+videos.forEach(video => {
+
+  const NewItem = TemplateVideo.cloneNode(true);
+  $_(".main-videos__item-video",NewItem).src = video.src
+  $_(".main-videos__item-title",NewItem).src = video.title
+
+  elVideoFragment.appendChild(NewItem)
+
+  ListVideo.appendChild(elVideoFragment)
+
+});
